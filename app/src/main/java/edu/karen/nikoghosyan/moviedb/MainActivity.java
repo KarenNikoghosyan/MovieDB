f370caa103cf3a8634778b98e428f980fc5e23f6
Karen Nikoghosyan <63981352+KarenNikoghosyan@users.noreply.github.com>
2021-04-08 22:45:57 +0300

    -Added 4 new xml animations. -Added fragment transition animations.

--- app/src/main/java/edu/karen/nikoghosyan/moviedb/MainActivity.java
+++ app/src/main/java/edu/karen/nikoghosyan/moviedb/MainActivity.java
@@ -1,21 +1,90 @@
 package edu.karen.nikoghosyan.moviedb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import edu.karen.nikoghosyan.moviedb.ui.favorite.FavoriteMovieFragment;
 import edu.karen.nikoghosyan.moviedb.ui.home.HomeMovieFragment;
+import edu.karen.nikoghosyan.moviedb.ui.search.SearchMovieFragment;
+import nl.joery.animatedbottombar.AnimatedBottomBar;
 
 public class MainActivity extends AppCompatActivity {
+    AnimatedBottomBar animatedBottomBar;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    Fragment fragment = null;
+
+    int currentPosition = 0;
+    int newPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         if (savedInstanceState == null) {
+            loadFragment();
+        }
+
+        animatedBottomBar = findViewById(R.id.bottom_bar);
+        animatedBottomBar.setOnTabSelectListener(new AnimatedBottomBar.OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int lastIndex, @Nullable AnimatedBottomBar.Tab lastTab, int newIndex, @NotNull AnimatedBottomBar.Tab newTab) {
+
+                int id = newTab.getId();
+                
+                if (id == R.id.nav_home) {
+                    fragment = new HomeMovieFragment();
+                    newPosition = 1;
+                } else if (id == R.id.nav_search) {
+                    fragment = new SearchMovieFragment();
+                    newPosition = 2;
+                } else if (id == R.id.nav_favorite) {
+                    fragment = new FavoriteMovieFragment();
+                    newPosition = 3;
+                }
+
+                loadFragment();
+                currentPosition = newPosition;
+            }
+
+            @Override
+            public void onTabReselected(int i, @NotNull AnimatedBottomBar.Tab tab) {
+
+            }
+        });
+    }
+
+    private void loadFragment() {
+        if (currentPosition == 0) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragmentContainer, HomeMovieFragment.newInstance())
                     .commitNow();
+        }
+
+        if (fragment != null) {
+            if (currentPosition > newPosition) {
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left, R.anim.slide_in_left, R.anim.slide_out_right)
+                        .replace(R.id.fragmentContainer, fragment)
+                        .commit();
+            }
+            if (currentPosition < newPosition) {
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_right, R.anim.slide_in_right, R.anim.slide_out_left)
+                        .replace(R.id.fragmentContainer, fragment)
+                        .commit();
+            }
         }
     }
 }
